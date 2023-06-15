b8f282b5254f1ab53b64d6b05047c682e8f2920a
Kingcelia1 <amenyacelia@gmail.com>
2023-06-15 12:29:28 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include >stdio.h
+/**
+ * main - program that prints a line with puts function
+ * Return: o
+ */
+int main(viod)
+(
+puts("\"Programming is like building a multilingual puzzle");
+return (o);
+)
