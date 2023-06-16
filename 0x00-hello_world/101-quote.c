042c46d6bb4ebd8640d38c28f75258a58d635287
Kingcelia1 <amenyacelia@gmail.com>
2023-06-16 13:17:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry point
+ * main - This the main function
+ * description - The C program that prints exactly
  * Return 1 (Success)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+return (1);
 }
