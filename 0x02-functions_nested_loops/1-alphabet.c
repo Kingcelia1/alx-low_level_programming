59529836bf7bc1c240caa6d27c4b43d50b464671
Kingcelia1 <amenyacelia@gmail.com>
2023-06-21 13:58:53 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,14 @@
 /**
  * print_alphabet - print all alphabet in lowercase
  */
-void print_alphabet(void);
+void print_alphabet(void)
+
 {
-	char letter;
+	char c;
 	
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n')
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
