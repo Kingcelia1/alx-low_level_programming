8b062bf736b09d01db648b0766f5412751451d16
Kingcelia1 <amenyacelia@gmail.com>
2023-06-21 15:59:03 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,15 @@
 
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+	int i;
+	int j;
 
-	while (i <= 0)
+	for (j = 1; i <= 10; i++)
 	{
-	for (c = 'n'; c <= 'z'; c++)
-	{
-	_putchar(c);
-	_putchar('\n');
-	i++;
-	}
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
 	}
 }
-
