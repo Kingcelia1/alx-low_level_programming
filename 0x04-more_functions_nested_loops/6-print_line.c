5e1cfc7aba0d31412c23a118d4904be7e3ebd97b
Kingcelia1 <amenyacelia@gmail.com>
2023-06-23 16:01:38 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,24 +7,11 @@
 
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i, j;
+	int i;
 
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
-			}
-			_putchar('\n');
-		}
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
 	}
+	_putchar('\n');
 }
